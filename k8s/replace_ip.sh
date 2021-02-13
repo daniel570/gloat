6b3d147190c031eca3157d173a1263a7374e0641
Daniel Harsheffer <danielharsheffer@gmail.com>
2021-02-13 09:30:21 -0500

    modified Jenkinsfile

--- k8s/replace_ip.sh
+++ k8s/replace_ip.sh
@@ -3,4 +3,4 @@
 newip=$(cat hosts.lst)
 
 echo $newip
-sed -i -e 's/[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}/8.8.8.2/g'   tsunami_job.yaml
+sed -i -e 's/[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}.[0-9]\{1,3\}/'$newip'/g'   tsunami_job.yaml
